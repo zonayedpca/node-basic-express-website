5f2fc2690ea464f0ca8bb921725becf4cb0d2dff
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-02-27 13:47:44 +0530

    basic express website functionality done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express'),
-      app = express();
-const posts = require('./models/demopost');
+      app = express(),
+      bodyParser = require('body-parser'),
+      posts = require('./models/demopost');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -14,9 +17,20 @@ app.get('/about', (req, res) => res.render('about'));
 
 app.get('/contact', (req, res) => res.render('contact'));
 
+app.post('/contact/contactinfo', (req, res) => {
+  const contact = req.body.contact;
+  res.render('contact_info', {contact});
+});
+
 app.post('/contact', (req, res) => console.log(req.body));
 
 app.get('/blog', (req, res) => res.render('blog', {posts}));
 
+app.post('/blog', (req, res) => {
+    posts.push(req.body.blog);
+    res.redirect('/blog');
+});
+
+app.get('/blog/new', (req, res) => res.render('addnew', {}));
+
 app.listen(process.env.PORT, process.env.IP, () => console.log('Server is running...'));
-// app.listen(3000, () => console.log('Server is running...'));
