352b8b057a0ef0d3ca30747596eec03830e0df2a
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-02-27 07:42:40 +0530

    page updates

--- app.js
+++ app.js
@@ -10,4 +10,13 @@ app.get('/', (req, res) => res.render('home', {posts}));
 
 app.get('/home', (req, res) => res.redirect('/'));
 
+app.get('/about', (req, res) => res.render('about'));
+
+app.get('/contact', (req, res) => res.render('contact'));
+
+app.post('/contact', (req, res) => console.log(req.body));
+
+app.get('/blog', (req, res) => res.render('blog', {posts}));
+
 app.listen(process.env.PORT, process.env.IP, () => console.log('Server is running...'));
+// app.listen(3000, () => console.log('Server is running...'));
